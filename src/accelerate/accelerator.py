1ba6951e4fd1127acda87b15a624f365f60a05aa
Dan Saunders <danjsaund@gmail.com>
2025-07-11 03:41:11 +0000

    remove logs

--- src/accelerate/accelerator.py
+++ src/accelerate/accelerator.py
@@ -1341,10 +1341,6 @@ class Accelerator:
         ... )
         ```
         """
-        print("in prepare")
-        
-        # import ipdb; ipdb.set_trace()
-
         if device_placement is None:
             device_placement = [None for _ in args]
         elif self.distributed_type in (DistributedType.DEEPSPEED, DistributedType.MEGATRON_LM):
@@ -1825,8 +1821,6 @@ class Accelerator:
         return model
 
     def _prepare_ao(self, *args):
-        print("in _prepare_ao")
-
         if not is_torchao_available():
             raise ImportError(
                 "`torchao` was not found on your system or is too old of a version. Please ensure that `torchao >= 0.6.1` is installed"
@@ -1836,20 +1830,12 @@ class Accelerator:
             models = [x for x in args if isinstance(x, torch.nn.Module)]
             optimizers = [x for x in args if isinstance(x, torch.optim.Optimizer)]
         for arg in args:
-            print("here2")
-            print(arg)
             if isinstance(arg, torch.nn.Module):
-                print("before")
-                print(arg)
-                print()
                 convert_model_to_fp8_ao(
                     arg,
                     config=self.ao_recipe_handler.config,
                     module_filter_func=self.ao_recipe_handler.module_filter_func,
                 )
-                print("after")
-                print(arg)
-                print()
 
         # Invariant: with FSDP2, optimizer is always passed to `prepare()` together with model
         # We only precompute scales if float8 all gather is enabled, possibly can add a flag for this later
